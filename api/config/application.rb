3cad2e1fa9e50e51e22aefe66ba0792ec3f7a13c
hikarucraft <147622480+hikarucraft@users.noreply.github.com>
2024-08-27 12:49:37 +0900

    setup: configured not to be automatically generated assets files when 'rails generate'

--- api/config/application.rb
+++ api/config/application.rb
@@ -28,5 +28,9 @@ module Api
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.generators do |g| # ここから追記
+      g.assets false          # CSS, JavaScriptファイルを自動生成しない
+      g.helper false      # helperファイルを自動生成しない
+    end
   end
 end
